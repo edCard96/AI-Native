63a99a21b967a41c766b19af9f884ec971fb7c17
Edgar Cardona <edgar.cardona@pursuit.org>
2025-06-15 14:22:30 -0400

    Add initial files: Hello World! script and My AI Journey Begins HTML page

---
+++ Hello World!.py
@@ -0,0 +1,14 @@
+# This python script prints a greeting message to the console
+
+print("Hello my name is Edgar Cardona. Welcome.")
+
+# Ask the user the question
+answer = input("What is 1+1? ")
+
+# Check if the answer is correct
+if answer == "2":
+    print("You got it! Two is the magic number.")
+else:
+    print("Not quite! Give it another try.")
+
+
