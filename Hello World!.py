2473c391926890ffc721f9c089a64eb0e22d6adf
edCard96 <edgar.cardona@pursuit.org>
2025-06-18 18:30:20 -0400

    Your commit message

--- Hello World!.py
+++ Hello World!.py
@@ -1,7 +1,10 @@
-# This is a simple Hello World program
+# This is a simple Hello World program that asks for your name
 
 # Ask the user for their name
 name = input("Please enter your name: ")
 
-# Print a personalized greeting using the name variable
-print(f"Hello, {name}!")
+# Check if the name is [Your Name] and print appropriate greeting
+if name == "Richard Noggin":
+    print("Hey, it's the awesome AI Director, Richard Noggin!")
+else:
+    print(f"Hello, {name}! Nice to meet you!")
